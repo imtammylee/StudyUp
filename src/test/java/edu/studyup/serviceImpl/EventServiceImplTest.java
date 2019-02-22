4cd9b1ad6e9cc9a91b80d47481655974e0c4b282
School <school@campus-054-120.ucdavis.edu>
2019-02-21 18:36:09 -0800

    Take out the error in test file

--- src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
+++ src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
@@ -298,7 +298,8 @@ class EventServiceImplTest {
 		event.setName("Event 7");
 		Location location = new Location(0, 0);
 		event.setLocation(location);
-		eventStudents.add(student);
+		List<Student> eventStudents = new ArrayList<>();;
+		eventStudents .add(student);
 		DataStorage.eventData.put(event.getEventID(), event);
 		//Create Student2 for Event 7
 		Student student2 = new Student();
