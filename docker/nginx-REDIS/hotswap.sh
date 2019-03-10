1593584ecdce388298a40a61f54a9ec2ff2fa79b
tttlee <tttlee@ucdavis.edu>
2019-03-10 07:34:20 +0000

    hotswap.sh normal cred

--- docker/nginx-REDIS/hotswap.sh
+++ docker/nginx-REDIS/hotswap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-sed -i'' "s/%{REDIS_HOST}/$1/" /etc/nginx/nginx.conf
+sed -i'' "s/server .*:6379/server $1:6379/" /etc/nginx/nginx.conf
 exec /usr/sbin/nginx -s reload
 
 exec "$@"
